9f5e7eeb44d9342b05d94788c757532558bb3944
mlloyd23 <melanie.m.lloyd@gmail.com>
2018-01-11 10:44:06 -0500

    add assignment 3 r script

---
+++ 3 intro to R.R
@@ -0,0 +1,34 @@
+##Assignment 3: Intro to R
+##Your name
+
+#I have provided an excel file that contains counts of each of the four colors 
+#from the 10x10 square with random placement.
+#Open that file in excel and save it as a .txt file
+
+#Read the data into R and assign it to a variable
+data<-
+
+
+#Assign specific columnso of data to variables to hold the number of dots of each color 
+pink<-
+blue<-
+green<-
+red<-
+
+
+##Using the mean() function, calculate Mean dots/quadrat for each color
+
+
+##Using the var() function, calculate Variance for each color 
+
+
+##Using the sd() function, calculate Standard Deviation for each color
+
+
+##Using the std.error() function (in the plotrix package)
+  
+  
+##Calculate Standard Error for each color
+
+
+##Calculate Variance:Mean ratio for each color
