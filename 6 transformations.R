bb798338c1aec913c003709fd90dc03cc0611ddb
mlloyd23 <melanie.m.lloyd@gmail.com>
2018-01-21 14:49:07 -0500

    add assignment 6

---
+++ 6 transformations.R
@@ -0,0 +1,42 @@
+##Assignment 6:Transformations
+##Your name
+
+##Here is a randomly generated data frame with a poisson distirbution
+set.seed(124)
+data<-rpois(1000, 3)
+data
+
+
+##Visual and quantitative tests of normality
+##Plot a histogram and Q-Q plot
+
+
+##shapiro-wilk normality test
+
+
+##Perform a square root transformation
+
+
+##Plot the histogram
+
+
+##shapiro-wilk test
+
+
+##Perform a log+1 transformation
+
+
+##Plot the histogram
+
+
+##shapiro-wilk test
+
+
+##Perform arcsine-square root transformation
+
+
+##Plot the histogram
+
+##Tukey's Ladder of Powers transformation
+
+#Plot histogram
