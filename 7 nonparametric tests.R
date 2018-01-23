c233ab1669d408519eec44f7b5d78f827ef42b1b
mlloyd23 <melanie.m.lloyd@gmail.com>
2018-01-23 13:20:32 -0500

    add assignment 7

---
+++ 7 nonparametric tests.R
@@ -0,0 +1,23 @@
+##Assignment 7
+##Your name
+
+##read in the seed dispersal data
+
+##Make histograms of weight, time, and distance
+
+##I'm curious about the difference in distance between bracts with different
+##number of seeds. Run a traditional ANOVA to look for a difference in distance 
+##traveled between bracts with 1, 2, 3, or 4 seeds 
+
+##Make a boxplot of the data
+
+##Now try to transform the data 
+
+##Make a historgram of each transformation and pick the best one
+
+##Re-run the ANOVA on the transformed data, is it different?
+
+##Now run the non-parametric version. Is it different?
+
+
+
