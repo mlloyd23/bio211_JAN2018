08dae10d313d0a2bda1752d574ed7cdd33806796
mlloyd23 <melanie.m.lloyd@gmail.com>
2018-01-25 12:01:41 -0500

    add assignment 8

---
+++ 8 correlations.R
@@ -0,0 +1,22 @@
+##Assignment 8
+##Your name
+
+#As a continuation from assignment 7, run a tukey's post hoc test on 
+#the traditional anova you ran with transfromed data to look for the difference
+#in distance traveled depending on seed number (continue with the subsetted 
+#data with 1,2,3,4 seeds)
+#You will need to make the Seeds column a factor
+
+
+#Now run a dunn's post hoc test on the Kruskal-Wallis test you ran
+
+
+#We looked at the correlation between distance and time in class
+#Now, look at the correlation between distance and weight
+#Use the traditional Pearson's test as well as Spearman's
+
+#Make a scatterplot of this relationship
+
+#Using the cars dataset, run a linear regression to look for the effect
+#of stopping distance on speed
+head(cars)
