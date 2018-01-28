f6ba9febd58f63cb03693a35ebfc7c963ead0713
mlloyd23 <melanie.m.lloyd@gmail.com>
2018-01-28 12:20:35 -0500

    add assignment 9

---
+++ 9 linear models.R
@@ -0,0 +1,32 @@
+##Assignment 9
+##Your name
+library(car)
+head(Prestige)
+
+#Average education of occupational incumbents, years, in 1971. 
+#Average income of incumbents, dollars, in 1971.
+#Percentage of incumbents who are women.
+#Pineo-Porter prestige score for occupation, from a social survey
+#Canadian Census occupational code.
+#Type of occupation.
+
+#I'm curious is years of education is a good predictor of income
+#plot education on the x axis and income on the y axis
+
+#Make a linear model to test if education is a good predictor of income
+
+#what is the y intercept of this model?
+#what is the slope of the best fit line?
+#what is the p value of the model?
+
+#Look at the diagnostic plots
+
+#what can you conclude about the fit of the model based on these plots?
+
+#are there any outliers?
+#what are they? (should be a name of a profession)
+
+#plot the data and the best fit line
+
+#predict income amounts based on 10 levels of education (you can pick which 10)
+     
